f4c3af88278e0525c6e09b67251e48644c88d005
ishabelle <kolodzinska.izabela@gmail.com>
2021-04-17 16:56:21 +0200

    add files

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,18 @@
 import { dom } from "./dom.js";
 
+function openBoard(board_id){
+    dom.loadBoard(board_id);
+}
+
 // This function is to initialize the application
 function init() {
     // init data
     dom.init();
     // loads the boards to the screen
     dom.loadBoards();
-
 }
 
 init();
+
+
+
