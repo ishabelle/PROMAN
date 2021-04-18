639ca15bcd55f6ccf40468c538f2e71b521044e1
ishabelle <kolodzinska.izabela@gmail.com>
2021-04-18 10:24:31 +0200

    refactor

--- main.py
+++ main.py
@@ -1,190 +1,180 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request
+
 from util import json_response
+import util
 
 import data_handler
-
 
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
-    """
-    This is a one-pager which shows all the boards and cards
-    """
     return render_template('index.html')
 
 
 @app.route("/get-boards")
 @json_response
 def get_boards():
-    """
-    All the boards
-    """
-    return data_handler.get_boards()
+    return data_handler.get_all_from_table('boards')
+
+
+@app.route("/get-cards")
+@json_response
+def get_all_cards():
+    return data_handler.get_all_from_table('cards')
 
 
 @app.route("/get-statuses")
 @json_response
 def get_statuses():
-    """
-    Get all statuses == amount of columns in each board
-    """
-    return data_handler.get_statuses()
+    return data_handler.get_all_from_table('statuses')
 
 
-@app.route("/get-status/<int:status_id>")
-@json_response
-def get_status(status_id):
-    """
-    Get all statuses == amount of columns in each board
-    """
-    return data_handler.get_status(status_id)
-
-
-@app.route("/get-statuses/<int:board_id>")
-@json_response
-def get_statuses_board(board_id):
-    """
-    Get all statuses == amount of columns in each board
-    """
-    return data_handler.get_statutes_board(board_id)
-
-
-@app.route("/get-cards/<int:board_id>")
-@json_response
-def get_cards_for_board(board_id: int):
-    """
-    All cards that belongs to a board
-    :param board_id: id of the parent board
-    """
-    return data_handler.get_cards_for_board(board_id)
-
-
-@app.route("/save-board-title/<int:board_id>", methods=["POST"])
-def save_board_title(board_id):
-    if request.method == 'POST':
-
-        title = request.form['title']
-        data_handler.update_board_title(board_id, title)
-        return redirect(url_for('index'))
-
-
-@app.route("/change-board-title/<board_id>", methods=["POST"])
-@json_response
-def change_board_title(board_id):
-    if request.method == 'POST':
-        data = request.json
-        title = data["title"]
-        board_id = board_id_conversion(board_id)
-        data_handler.update_board_title(board_id, title)
-        return board_id
-
-
-@app.route("/change-status-title/", methods=["POST"])
-@json_response
-def change_status_title():
-    if request.method == 'POST':
-        data = request.json
-        title = data["title"]
-        status_id = data["status_id"]
-        return data_handler.update_status_title(status_id, title)
-
-
-@app.route("/change-card-title/<cardId>", methods=["POST"])
-@json_response
-def change_card_title(cardId):
-    if request.method == 'POST':
-        data = request.json
-        id = board_id_conversion(data['cardId'])
-        print(id)
-        title = data['cardNewTitle']
-        data_handler.card_title_change(id, title)
-        return ""
-
-
-@app.route("/update-card-status/<int:card_id>", methods=["POST"])
-@json_response
-def change_card_status(card_id):
-    if request.method == 'POST':
-        data = request.json
-        status_id = data['status_id']
-        data_handler.update_card_status(card_id, status_id)
-
-
-@app.route("/delete-board/<board_id>", methods=["POST"])
-@json_response
-def delete_board(board_id):
-    if request.method == 'POST':
-        board_id = board_id_conversion(board_id)
-        data_handler.delete_board(board_id)
-        return board_id
-
-
-def board_id_conversion(data):
-    data_converted = data.split("_")
-    print(data)
-    print(data_converted)
-    if len(data_converted) == 1:
-        list_of_char = list(data)
-        list_of_digits = []
-        for char in list_of_char:
-            if char.isdigit():
-                list_of_digits.append(char)
-        return "".join(list_of_digits)
-    else:
-        list_of_char = list(data_converted[1])
-        list_of_digits = []
-        for char in list_of_char:
-            if char.isdigit():
-                list_of_digits.append(char)
-        return "".join(list_of_digits)
-
-
-@app.route("/add-new-card/<boardId>", methods=["POST"])
-@json_response
-def add_new_card(boardId):
-    if request.method == 'POST':
-        data = request.json
-        return data_handler.add_new_card(data)
-
-
-@app.route("/add-new-column/<int:boardId>/", methods=["POST"])
-@json_response
-def add_new_column(boardId):
-    if request.method == 'POST':
-        data = request.json
-        test = data_handler.add_new_column(data)
-        print(test)
-        return test
-
-
-@app.route("/delete-card/<int:cardId>", methods=["POST"])
-@json_response
-def delete_card(cardId):
-    if request.method == 'POST':
-        return data_handler.delete_card(cardId)
-
-
-@app.route("/delete-column/<int:board_id>", methods=["POST"])
-@json_response
-def delete_column(board_id):
-    if request.method == 'POST':
-        return data_handler.delete_column(board_id, request.json)
-
-
-@app.route("/create-new-board", methods=["POST"])
+@app.route('/create-new-board', methods=['GET', 'POST'])
 @json_response
 def create_new_board():
-    if request.method == 'POST':
-        data = request.json
-        data_handler.add_board(data)
-        id = str(data_handler.greatest_board_id())
-        return id
+    data_handler.create_new_board()
+    top_board = data_handler.get_last_board()
+    data_handler.create_status(top_board[0]['id'])
+    return top_board
+
+
+@app.route('/create-private-board', methods=['GET', 'POST'])
+@json_response
+def create_private_board():
+    data = request.get_json()
+    data_handler.create_new_board(int(data['owner']))
+    top_board = data_handler.get_last_board()
+    data_handler.create_status(top_board[0]['id'])
+    return top_board
+
+
+@app.route("/create-card", methods=["GET", "POST"])
+@json_response
+def create_card():
+    data = request.get_json()
+    data_handler.create_card(data["board_id"], data["status_id"])
+    return data_handler.get_all_from_table('cards')
+
+
+@app.route("/delete-card", methods=["GET", "POST"])
+@json_response
+def delete_card():
+    card_id = request.get_json()
+    response = data_handler.delete_card(card_id)
+    return response
+
+
+@app.route("/rename", methods=['GET', 'POST'])
+@json_response
+def rename():
+    data = request.get_json()
+    response = data_handler.rename_board(data["title"], data["id"])
+    return response
+
+
+@app.route('/drag&drop', methods=['GET', 'POST'])
+@json_response
+def drag_and_drop():
+    data = request.get_json()
+    response = data_handler.update_status(data['new_id'], data['old_id'])
+    return response
+
+
+@app.route("/rename-status", methods=['GET', 'POST'])
+@json_response
+def rename_status():
+    data = request.get_json()
+    response = data_handler.rename_status(data["title"], data["id"])
+    return response
+
+
+@app.route("/rename-card", methods=['GET', 'POST'])
+@json_response
+def rename_card():
+    data = request.get_json()
+    response = data_handler.rename_card(data["title"], data["id"])
+    return response
+
+
+@app.route("/create-status", methods=["GET", "POST"])
+def create_status():
+    data = request.get_json()
+    data_handler.add_status(data["board_id"])
+    return data_handler.get_last_status()[0]
+
+
+@app.route("/delete-board", methods=["GET", "POST"])
+@json_response
+def delete_board():
+    board_id = request.get_json()
+    response = data_handler.delete_board(board_id)
+    return response
+
+
+@app.route("/board-open-close", methods=["GET", "POST"])
+@json_response
+def board_open_close():
+    data = request.get_json()
+    response = data_handler.change_board_open_close(data['boolean'], data['id'])
+    return response
+
+
+@app.route('/check_username', methods=['GET', 'POST'])
+@json_response
+def check_username():
+    data = request.get_json()
+    response = data_handler.check_user_data('username', data['username'])
+    return response
+
+
+@app.route('/check_email', methods=['GET', 'POST'])
+@json_response
+def check_email():
+    data = request.get_json()
+    response = data_handler.check_user_data('email_address', data['email'])
+    return response
+
+
+@app.route('/check_passwords', methods=['GET', 'POST'])
+@json_response
+def check_passwords():
+    data = request.get_json()
+    psw = util.hash_password(data['psw'])
+    if not util.verify_password(data['pswAgain'], psw):
+        return 'True'
+    else:
+        return 'False'
+
+
+@app.route('/save_data', methods=['GET', 'POST'])
+@json_response
+def save_data():
+    data = request.get_json()
+    psw = util.hash_password(data['password'])
+    data_handler.save_data(data['username'], data['email'], psw)
+    return 'done'
+
+
+@app.route('/check_login', methods=['GET', 'POST'])
+@json_response
+def check_login():
+    data = request.get_json()
+    if data_handler.check_user_data('username', data['username']) == 'True':
+        real_psw = data_handler.password_by_username(data['username'])
+        if not util.verify_password(data['password'], real_psw[0]['password']):
+            return 'False'
+        else:
+            return real_psw[0]['id']
+    else:
+        return 'False'
 
 
 def main():
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5008)
+
     # Serving the favicon
     with app.app_context():
         app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon/favicon.ico'))
