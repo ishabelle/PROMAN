8855d459af73024f525b219bc5c6fe449694d2c2
ishabelle <kolodzinska.izabela@gmail.com>
2021-04-18 10:25:40 +0200

    refactor

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 from functools import wraps
+
+import bcrypt
 from flask import jsonify
 
 
@@ -22,3 +24,4 @@ def hash_password(plain_text_password):
 def verify_password(plain_text_password, hashed_password):
     hashed_bytes_password = hashed_password.encode('utf-8')
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
